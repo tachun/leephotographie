6401a6f0a42cb4b4eb3f4d5d6e9ce974458fd6d7
tachun lin <creed88@gmail.com>
2013-05-22 23:32:12 +0200

    Add wufoo form

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,6 +6,27 @@ $(function() {
     speed: 1200,
     manualControls: '#slider-pager'
   });
+
+  function initialize() {
+    var mapOptions = {
+      zoom: 17,
+      center: new google.maps.LatLng(48.875286, 2.360062),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+      
+    };
+
+    var stylez = [{
+      featureType: "all",
+      elementType: "all",
+      stylers: [
+        { saturation: -100 } // <-- THIS
+      ]}
+    ];
+
+    map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions, stylez);
+  }
+  google.maps.event.addDomListener(window, 'load', initialize);
 });
 
 function openPhoto(){
