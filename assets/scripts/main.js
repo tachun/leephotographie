72b9f6258cfd9a2dbd2982b88765737a433c8b01
tachun lin <creed88@gmail.com>
2013-06-09 10:28:59 +0200

    Finish login pannel

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,32 +1,35 @@
 $(function() {
   openPhoto();
   initSubmenu();
+
   $("#slider").responsiveSlides({
     auto: true,
     speed: 1200,
     manualControls: '#slider-pager'
   });
 
-  function initialize() {
-    var mapOptions = {
-      zoom: 17,
-      center: new google.maps.LatLng(48.875286, 2.360062),
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+  Parse.initialize("wNc5CugL4O230OuLnYVEOm0HyYJUo6JQIxF2W5jV", "YXM31sd1kgj6dRslfPF8tUQHJ6oSTpJjqYJFTRMG");
+  
+  // function initialize() {
+  //   var mapOptions = {
+  //     zoom: 17,
+  //     center: new google.maps.LatLng(48.875286, 2.360062),
+  //     mapTypeId: google.maps.MapTypeId.ROADMAP
       
-    };
+  //   };
 
-    var stylez = [{
-      featureType: "all",
-      elementType: "all",
-      stylers: [
-        { saturation: -100 } // <-- THIS
-      ]}
-    ];
+  //   var stylez = [{
+  //     featureType: "all",
+  //     elementType: "all",
+  //     stylers: [
+  //       { saturation: -100 } // <-- THIS
+  //     ]}
+  //   ];
 
-    map = new google.maps.Map(document.getElementById('map-canvas'),
-        mapOptions, stylez);
-  }
-  google.maps.event.addDomListener(window, 'load', initialize);
+  //   map = new google.maps.Map(document.getElementById('map-canvas'),
+  //       mapOptions, stylez);
+  // }
+  // google.maps.event.addDomListener(window, 'load', initialize);
 });
 
 function openPhoto(){
